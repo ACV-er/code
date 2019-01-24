bcaa0b51840e654c5463f0a57d7393d2b5a1cfd1
dhd <1246009411@qq.com>
2019-01-24 23:02:28 +0800

    随机数组算法

--- C/算导/随机排列数组.c
+++ C/算导/随机排列数组.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
 len 返回数组的长度
 
 返回值 指向随机动态数组的指针
 */
-int * rand_array(int len) {
+int* rand_array(int len)
+{
+    if (len < 1) {
+        return NULL;
+    }
     int i, t, tmp;
-    int* rand_array = (int *)malloc(sizeof(int) * len);
-    for(i=0; i<len; i++) {
+    int* rand_array = (int*)malloc(sizeof(int) * len);
+    for (i = 0; i < len; i++) {
         rand_array[i] = i;
     }
-    for(i=0; i<len; i++) {
+    for (i = 0; i < len; i++) {
         tmp = rand() % len;
         t = rand_array[i];
         rand_array[i] = rand_array[tmp];
@@ -25,10 +27,11 @@ int * rand_array(int len) {
     return rand_array;
 }
 
-int main(void) {
+int main(void)
+{
     srand((unsigned)time(NULL));
-    int * a = rand_array(10);
-    for(int i=0; i<10; i++) {
+    int* a = rand_array(10);
+    for (int i = 0; i < 10; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
